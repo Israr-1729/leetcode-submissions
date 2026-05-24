eb8f42ea22f73fdecbabeaf444a09adb6106f62b
Israr-1729 <idnac1729@gmail.com>
2026-05-24 22:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (86.83%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+int value(char c) {
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        return 1000; // M
+    }
+
+
+    int romanToInt(string s) {
+        int size = s.size();
+        int ans = 0;
+        for(int i = 0; i<size-1; i++)
+        {
+            if(value(s[i])<value(s[i+1]))
+            ans-=value(s[i]);
+            else
+            ans+=value(s[i]);
+        }
+        ans+=value(s[size-1]);
+        return ans;
+    }
+};
