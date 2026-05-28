33da72003a56e0431bd828f4ece4cd749f4a9bd7
Israr-1729 <idnac1729@gmail.com>
2026-05-28 08:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.37%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> alphabets(26, 0);
+        int size = sentence.size();
+        for(int i = 0; i<size; i++)
+        {
+            alphabets[sentence[i]-'a']++;
+        }
+        for(int i = 0; i<26; i++)
+        {
+            if(alphabets[i]==0)
+            return false;
+        }
+        return true;
+    }
+};
