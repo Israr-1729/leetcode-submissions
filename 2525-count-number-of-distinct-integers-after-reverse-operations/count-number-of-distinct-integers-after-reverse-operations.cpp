bc69e26dfd54f9bca498c86c500895cc6d144624
Israr-1729 <idnac1729@gmail.com>
2026-05-30 17:42:50 +0530

    Time: 401 ms (5.30%) | Memory: 118.6 MB (6.97%) - LeetSync

---
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countDistinctIntegers(vector<int>& nums) {
+        int size = nums.size();
+        vector<int>numsAndReversed(size*2);
+        for(int i = 0; i<size; i++)
+        {
+            numsAndReversed[i] = nums[i];
+        }
+        for(int i = size; i<size*2; i++)
+        {
+            int copy = nums[i-size];
+            int reversed = 0;
+            while(copy!=0)
+            {
+            reversed = reversed*10 + copy%10;
+            copy/=10;
+            }
+            numsAndReversed[i] = reversed;
+        }
+        int count = 0;
+        set<int> isSeen;
+        for(int i = 0; i<size*2; i++)
+        {
+            int num = numsAndReversed[i]; 
+            if(isSeen.find(num) == isSeen.end())
+            {
+                count++;
+                isSeen.insert(num);
+            }
+        }
+        return count;
+    }
+};
