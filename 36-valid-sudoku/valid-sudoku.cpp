cd6a7ed9e289100f2611b8b489ebe2cbf119ad6a
Israr-1729 <idnac1729@gmail.com>
2026-06-04 21:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (58.68%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i = 0; i<9; i++)
+        {
+            set<char> isSeen;
+            for(int j = 0; j<9; j++)
+            {
+                if(board[i][j]=='.')
+                continue;
+                if(isSeen.find(board[i][j])!=isSeen.end())
+                return false;
+                isSeen.insert(board[i][j]);
+            }
+        }
+        for(int i = 0; i<9; i++)
+        {
+            set<char> isSeen;
+            for(int j = 0; j<9; j++)
+            {
+                if(board[j][i]=='.')
+                continue;
+                if(isSeen.find(board[j][i])!=isSeen.end())
+                return false;
+                isSeen.insert(board[j][i]);
+            }
+        }
+
+for(int row = 0; row < 9; row += 3)
+{
+    for(int col = 0; col < 9; col += 3)
+    {
+        set<char> isSeen;
+
+        for(int i = row; i < row + 3; i++)
+        {
+            for(int j = col; j < col + 3; j++)
+            {
+                if(board[i][j] == '.')
+                    continue;
+
+                if(isSeen.find(board[i][j]) != isSeen.end())
+                    return false;
+
+                isSeen.insert(board[i][j]);
+            }
+        }
+    }
+}
+        return true;
+    }
+};
