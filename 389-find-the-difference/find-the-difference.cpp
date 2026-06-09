2bd1cee0a64dde11e840269aa9e135226640a695
Israr-1729 <idnac1729@gmail.com>
2026-06-09 22:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.55%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> frq(26);
-
+        int xo = 0;
         int size = s.size();
         for(int i = 0; i<size; i++)
-        {
-            frq[s[i]-'a']++;
-        }
-        for(int i = 0; i<size+1; i++)
-        {
-            frq[t[i]-'a']--;
-        }
-
-        for(int i = 0; i<26; i++)
-        {
-            if(frq[i]!=0)
-            return i+'a';
-        }
-    return 'a';     
+        xo = xo^s[i]^t[i];
+        xo = xo^t[size];
+        return xo;
     }
 };
