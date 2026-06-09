2db3458a831a0fe568bfa0514e9f67b31f48454a
Israr-1729 <idnac1729@gmail.com>
2026-06-10 00:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (91.24%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int minCost(int n) {
-
-        long long a = n-1;
-        long long b = n;
-
-        long long c = a*b/2;
-        return c;
+        return n*(n-1)/2;
         
     }
 };
