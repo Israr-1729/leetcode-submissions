7f9414860dace58bfc4697cbaa9d381325050f30
Israr-1729 <idnac1729@gmail.com>
2026-06-10 01:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.10%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int copy = x;
+        int sum = 0;
+        while(copy>0)
+        {
+            sum += copy%10;
+            copy/=10;
+        }
+
+        if(x%sum==0)
+        return sum;
+        return -1;
+        
+    }
+};
