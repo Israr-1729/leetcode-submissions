93eea88dd211478b057edb224c15766ffa08b80a
Israr-1729 <idnac1729@gmail.com>
2026-06-16 11:49:24 +0530

    Time: 7 ms (35.61%) | Memory: 39.8 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+        for(char c : s)
+        {
+            if (c>='a' && c<='z')
+            result += c;
+
+            else if(c=='*')
+            {
+                if(result.size()==0)
+                continue;
+                else
+                result.pop_back();
+            }
+
+            else if(c=='#')
+            {
+                result += result;
+            }
+
+            else if(c == '%')
+            {
+                reverse(result.begin(), result.end());
+            }
+        }
+        return result;
+    }
+};
