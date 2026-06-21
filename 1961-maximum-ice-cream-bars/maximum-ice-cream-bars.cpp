6612a3f48214b01b31de3c2ef311e60dd18b7ff3
Israr-1729 <idnac1729@gmail.com>
2026-06-21 05:48:20 +0530

    Time: 36 ms (42.67%) | Memory: 80.3 MB (37.51%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -3,16 +3,15 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         sort(costs.begin(), costs.end());
         int size = costs.size();
-        int sum = 0;
         int count = 0;
         for(int i = 0; i<size; i++)
         {
-            if(sum + costs[i]>coins)
+            if(costs[i]>coins)
             break;
             else
             {
-                sum += costs[i];
-                count ++;
+                count++;
+                coins -= costs[i];
             }
         }
         return count;
