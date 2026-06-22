42db6fd1a60b843596bc74e659d0d0e04d7bbc8b
Israr-1729 <idnac1729@gmail.com>
2026-06-23 00:56:55 +0530

    Time: 31 ms (75.29%) | Memory: 10 MB (56.47%) - LeetSync

---
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isVowel(char c)
+    {
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+
+    int beautifulSubstrings(string s, int k) {
+        int n = s.size();
+        int result = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int vowels = 0;
+            int consonants = 0;
+
+            for(int j = i; j < n; j++)
+            {
+                if(isVowel(s[j]))
+                    vowels++;
+                else
+                    consonants++;
+
+                if(vowels == consonants &&
+                   (vowels * consonants) % k == 0)
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+};
