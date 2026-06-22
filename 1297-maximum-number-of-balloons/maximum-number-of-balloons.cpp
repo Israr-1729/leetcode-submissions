d3c5a0f012933b07fb9e26abd00cdbb934ee1f5b
Israr-1729 <idnac1729@gmail.com>
2026-06-22 06:06:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (95.38%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        vector<int> freq (26);
+        vector<int> freq(26);
         for(char c : text)
         {
             freq[c-'a']++;
         }
 
-        int counter = 0;
-
+        int count = 0;
         while(true)
         {
-                freq['b'-'a']--;
-                freq['a'-'a']--;
-                freq['l'-'a']--;
-                freq['l'-'a']--;
-                freq['o'-'a']--;
-                freq['o'-'a']--;
-                freq['n'-'a']--;
-            if(freq['b'-'a']>=0 &&freq['a'-'a']>=0 &&freq['l'-'a']>=0 &&freq['o'-'a']>=0 &&freq['n'-'a']>=0)
-            {
-                counter++;
-            }
+            freq['b'-'a']--;
+            freq['a'-'a']--;
+            freq['l'-'a']--;
+            freq['l'-'a']--;
+            freq['o'-'a']--;
+            freq['o'-'a']--;
+            freq['n'-'a']--;
+
+            if(freq['b'-'a']>=0 && freq['a'-'a']>=0 && freq['l'-'a']>=0 && freq['o'-'a']>=0 && freq['n'-'a']>=0)
+            count++;
             else
             break;
         }
-        return counter;
+        return count;
     }
 };
