559365089417e7381fee3e6d91eb21da3241b30d
Israr-1729 <idnac1729@gmail.com>
2026-06-23 06:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (37.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,6 +3,13 @@ public:
     int fib(int n) {
         if(n<2)
         return n;
-        return fib(n-1)+fib(n-2);
+        int arr[2] = {0,1};
+        for(int i = 2; i<=n; i++)
+        {
+            int thisElement = arr[0] + arr[1];
+            arr[0] = arr[1];
+            arr[1] = thisElement;
+        }
+        return arr[1];
     }
 };
