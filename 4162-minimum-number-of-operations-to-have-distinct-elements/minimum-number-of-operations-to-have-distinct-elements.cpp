1c34e2d265659acae33e320148ac16c66076ba8b
Israr-1729 <idnac1729@gmail.com>
2026-06-24 22:59:16 +0530

    Time: 45 ms (92.26%) | Memory: 186.3 MB (71.73%) - LeetSync

---
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_set<int> st;
+
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (st.contains(nums[i])) {
+                return i / 3 + 1;
+            }
+            st.insert(nums[i]);
+        }
+
+        return 0;
+    }
+};
