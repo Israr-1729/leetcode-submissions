4c0c28d5b0b7e96d42e92da76b410dcb1e9a0110
Israr-1729 <idnac1729@gmail.com>
2026-06-28 06:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (80.20%) - LeetSync

---
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int count = 0;
+        for(int a : nums1)
+        {
+            for(int b : nums2)
+            {
+                if(a%(b*k)==0)
+                count++;
+            }
+        }
+        return count;
+    }
+};
