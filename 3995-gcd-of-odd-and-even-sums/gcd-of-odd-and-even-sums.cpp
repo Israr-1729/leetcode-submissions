65b1ac873e2b47bd569f8b4d97db4b0669832b28
Israr-1729 <idnac1729@gmail.com>
2026-07-15 05:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = n*n;
-        int sumEven = n*(n+1);
-
-        for(int i = sumOdd; i>=1; i--)
-        {
-            if(sumOdd%i == 0 && sumEven%i == 0)
-            return i;
-        }
-        return 1;
+        return n;
     }
 };
