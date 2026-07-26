33223990a56fa1f2ad41a84eedb63b21ee9033be
Israr-1729 <idnac1729@gmail.com>
2026-07-26 20:04:09 +0530

    Time: 22 ms (76.33%) | Memory: 32.2 MB (5.80%) - LeetSync

---
+++ 690-employee-importance/employee-importance.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        int i = 0; int size = employees.size();
+        for(i = 0; i<size; i++)
+        {
+            if(employees[i]->id==id)
+            break;
+        }
+
+        if(employees[i]->subordinates.size()==0)
+        return employees[i]->importance;
+
+        else
+        {
+            int importance = employees[i]->importance;
+            vector<int> subor = employees[i]->subordinates;
+            for(int a : subor)
+            {
+                importance += getImportance(employees, a);
+            }
+            return importance;
+        }
+    }
+};
