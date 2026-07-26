8846a88d5725be3bb6275c733caa14cff5988f7c
Israr-1729 <idnac1729@gmail.com>
2026-07-26 12:29:30 +0530

    Time: 3 ms (65.12%) | Memory: 31.5 MB (76.45%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,8 +1,47 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int size = nums.size();
-        return max(nums[size-1]*nums[size-2]*nums[size-3], nums[0]*nums[1]*nums[size-1]);
+        vector<int> large(3, INT_MIN);
+        vector<int> small(2, INT_MAX);
+
+        //small[0] = smallest. small[1] = 2nd smallest
+        for(int a : nums)
+        {
+            if(a<=small[0])
+            {
+                small[1] = small[0];
+                small[0] = a;
+            }
+
+            else if(a<small[1])
+            {
+                small[1] = a;
+            }
+        }
+
+        //large[0] = largest. large[1] = 2nd largest. large[2] = 3rd largest.
+
+        for(int a : nums)
+        {
+            if(a>=large[0])
+            {
+                large[2] = large[1];
+                large[1] = large[0];
+                large[0] = a;
+            }
+
+            else if(a>=large[1])
+            {
+                large[2] = large[1];
+                large[1] = a;
+            }
+
+            else if(a>large[2])
+            {
+                large[2] = a;
+            }
+
+        }
+        return max(large[0]*large[1]*large[2], small[0]*small[1]*large[0]);
     }
 };
