eabddc793af716a3e3079a9cc0c9129f8daa08bc
Israr-1729 <idnac1729@gmail.com>
2026-07-27 21:54:39 +0530

    Time: 12 ms (43.56%) | Memory: 25.5 MB (73.08%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,69 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char, int> mp;
-        unordered_set<char> letters;
-        for(char c : word)
-        {
-            mp[c]++;
-            letters.insert(c);
+        vector<int> freq(26, 0);
+
+        for (char c : word)
+            freq[c - 'a']++;
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int ans = 0;
+
+        for (int i = 0; i < 26 && freq[i] > 0; i++) {
+            ans += freq[i] * (i / 8 + 1);
         }
 
-        vector<char> sortedLetters;
-        for(char c : letters)
-        sortedLetters.push_back(c);
-
-        sort(sortedLetters.begin(), sortedLetters.end(), [&](char a, char b) { return mp[a] > mp[b];});
-
-        unordered_set<char> pushOnce;
-        unordered_set<char> pushTwice;
-        unordered_set<char> pushThrice;
-        unordered_set<char> pushFourTimes;
-
-        int size = sortedLetters.size();
-        int idx = 0;
-
-        while(idx<size && idx<8)
-        {
-            pushOnce.insert(sortedLetters[idx]);
-            idx++;
-        }
-
-        while(idx<size && idx<16)
-        {
-            pushTwice.insert(sortedLetters[idx]);
-            idx++;
-        }
-
-        while(idx<size && idx<24)
-        {
-            pushThrice.insert(sortedLetters[idx]);
-            idx++;
-        }
-
-        while(idx<size && idx<32)
-        {
-            pushFourTimes.insert(sortedLetters[idx]);
-            idx++;
-        }
-
-
-        int result = 0;
-        for(char c : word)
-        {
-            if(pushOnce.contains(c))
-            result++;
-
-            else if(pushTwice.contains(c))
-            result += 2;
-
-            else if(pushThrice.contains(c))
-            result += 3;
-
-            else
-            result += 4;
-        }
-
-        return result;
+        return ans;
     }
 };
