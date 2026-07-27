fef4da2352a4a0f144cd8a2ce11d8dabfdd6574e
Israr-1729 <idnac1729@gmail.com>
2026-07-27 06:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (29.01%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        set<int> copy;
+        long long max1 = LLONG_MIN; long long max2 = LLONG_MIN; long long max3 = LLONG_MIN;
+
         for(int a : nums)
-        copy.insert(a);
+        {
+            if(a>max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = a;
+            }
 
-        vector<int> sorted;
-        for(auto &a : copy)
-        sorted.push_back(a);
+            else if(a>max2 && a!=max1)
+            {
+                max3 = max2;
+                max2 = a;
+            }
 
-        if(sorted.size()<3)
-        return sorted[sorted.size()-1];
-
-        else
-        return sorted[sorted.size()-3];
+            else if(a>max3 && a!=max2 && a!=max1)
+            {
+                max3 = a;
+            }
+        }
+        return max3 == LLONG_MIN ? max1 : max3;
     }
 };
