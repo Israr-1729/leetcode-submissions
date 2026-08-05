f7d638f8453e73fa12a03a77f2d818ccb2c7b16c
Israr-1729 <idnac1729@gmail.com>
2026-08-05 14:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (71.49%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
-        unordered_set<string> indices;
         int size = nums.size();
+        int count = 0;
         for(int i = 0; i < size-1; i++)
         {
             for(int j = i+1; j < size; j++)
             {
                 if(abs(nums[i] - nums[j]) == k)
                 {
-                    string toAdd = "";
-                    toAdd += 'a' + i;
-                    toAdd += 'a' + j;
-
-                    indices.insert(toAdd);
+                    count++;
                 }
             }
         }
-        return indices.size();
+        return count;
     }
 };
