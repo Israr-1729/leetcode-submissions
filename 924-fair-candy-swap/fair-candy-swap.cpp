0369f4e55a01a20b7a30da096f4bcdb44562900b
Israr-1729 <idnac1729@gmail.com>
2026-08-05 21:28:34 +0530

    Time: 59 ms (16.25%) | Memory: 62.8 MB (5.69%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        long long totalAlice = 0;
+        long long totalBob = 0;
+
+        unordered_set<int> setAlice;
+        for(int a : aliceSizes)
+        {
+            totalAlice += a;
+            setAlice.insert(a);
+        }
+
+        unordered_set<int> setBob;
+        for(int a : bobSizes)
+        {
+            totalBob += a;
+            setBob.insert(a);
+        }
+
+        int final = (totalAlice + totalBob)/2;
+
+        for(int a : aliceSizes)
+        {
+            int aliceAfterGiven = totalAlice - a;
+            int neededForFair = final - aliceAfterGiven;
+
+            if(setBob.contains(neededForFair))
+            return {a, neededForFair};
+        }
+        return {-1, -1};
+    }
+};
