25fba797c80c729927698692774b915092e37d01
Israr-1729 <idnac1729@gmail.com>
2026-08-06 14:42:22 +0530

    Time: 44 ms (18.63%) | Memory: 22.9 MB (32.45%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -3,27 +3,31 @@ public:
 
     vector<int> primeNumbers;
 
-    bool isPrime (int num) {
-        if(num == 2)
-        return true;
+void fillPrimeNumbers()
+{
+    static bool isPrime[1001];
 
-        if(num < 2 || num % 2 == 0)
-        return false;
+    memset(isPrime, true, sizeof(isPrime));
+    isPrime[0] = isPrime[1] = false;
 
-        for(int i = 3; i*i <= num; i+=2) {
-            if(num % i == 0)
-            return false;
-        }
-        return true;
-    }
-
-    void fillPrimeNumbers() {
-        for(int i = 2; i <= 1000; i++)
+    for (int i = 2; i * i <= 1000; ++i)
+    {
+        if (isPrime[i])
         {
-            if(isPrime(i))
-            primeNumbers.push_back(i);
+            for (int j = i * i; j <= 1000; j += i)
+                isPrime[j] = false;
         }
     }
+
+    primeNumbers.clear();
+    primeNumbers.reserve(168);
+
+    for (int i = 2; i <= 1000; ++i)
+    {
+        if (isPrime[i])
+            primeNumbers.push_back(i);
+    }
+}
 
     int distinctPrimeFactors(vector<int>& nums) {
         fillPrimeNumbers();
