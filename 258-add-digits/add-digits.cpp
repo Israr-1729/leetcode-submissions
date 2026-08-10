19a5f5ab334e7d6cc279107885d451695fa69188
Israr-1729 <idnac1729@gmail.com>
2026-08-10 19:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (46.38%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num / 10 == 0)
+        return num; 
+        
+        int ans = 0;
+        while(num)
+        {
+            ans += num %10;
+            num/=10;
+        }
+        return addDigits(ans);
+    }
+};
