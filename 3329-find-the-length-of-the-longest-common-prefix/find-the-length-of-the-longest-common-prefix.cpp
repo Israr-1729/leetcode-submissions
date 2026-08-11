7de4d5aa47367357bfe3461edd6991020247b006
Israr-1729 <idnac1729@gmail.com>
2026-08-11 07:09:50 +0530

    Time: 212 ms (34.82%) | Memory: 158.8 MB (51.77%) - LeetSync

---
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        //Larger is arr2
+
+        unordered_set<int> copy2;
+        for(int a : arr2)
+        {
+            while(a)
+            {
+                copy2.insert(a);
+                a/=10;
+            }
+        }
+
+        vector<int> copy1;
+        for(int a : arr1)
+        {
+            while(a)
+            {
+                copy1.push_back(a);
+                a/=10;
+            }
+        }
+
+
+        int maxLength = 0;
+        for(int a : copy1)
+        {
+            if(copy2.contains(a) && to_string(a).size() > maxLength)
+            {
+                maxLength = to_string(a).size();
+            }
+        }
+    return maxLength;
+    }
+};
