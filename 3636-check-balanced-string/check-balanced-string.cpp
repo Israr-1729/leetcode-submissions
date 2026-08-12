3e006c5315f46d6aacc7a5f551e1bd0b1dae9a10
Israr-1729 <idnac1729@gmail.com>
2026-08-12 15:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.06%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int sumEven = 0;
+        int sumOdd = 0;
+        for(int i = 0; i<num.size(); i++)
+        {
+            if(i%2 == 0)
+            sumEven += num[i] - '0';
+
+            else
+            sumOdd += num[i] - '0';
+        }
+        return sumEven == sumOdd;
+    }
+};
