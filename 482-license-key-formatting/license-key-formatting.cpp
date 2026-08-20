6794926c098137f0a25271ab7065f8fdeb3c7572
Israr-1729 <idnac1729@gmail.com>
2026-08-20 19:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (29.46%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.cpp
+++ 482-license-key-formatting/license-key-formatting.cpp
@@ -1,55 +1,31 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-
-        bool allDash = true;
+        string withoutDashes = "";
         for(char c : s)
         {
-            if(c != '-')
-            {
-                allDash = false;
-                break;
-            }
-        }
-
-        if(allDash)
-        return "";
-
-        int alphaNumericCount = 0;
-        for(char c : s)
-        {
-            if(c != '-')
-            {
-                alphaNumericCount++;
-            }
+            if(c!='-')
+            withoutDashes.push_back(c);
         }
 
         string result = "";
-
-        int count = 0;
-        for(int i = s.size()-1; i >=0; i--)
+        int blockCount = 0;
+        for(int i = withoutDashes.size()-1; i >= 0; i--)
         {
-            if(s[i] == '-')
-            continue;
-
-            else
-            {
-                result.push_back(toupper(s[i]));
-            }
-            
-
-            count++;
-            if(count == k && i!=0)
+            result.push_back(toupper(withoutDashes[i]));
+            blockCount++;
+            if(blockCount == k)
             {
                 result.push_back('-');
-                count = 0;
+                blockCount = 0;
             }
         }
-
-
-        if(result[result.size()-1] == '-' && s != "-")
+        
+        if(result == "")
+        return result; 
+        
+        if(result.back() == '-')
         result.pop_back();
-
 
         reverse(result.begin(), result.end());
         return result;
