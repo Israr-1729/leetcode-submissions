9e4d5ad6144130da7cf7bf4a87f05146960ddd06
Israr-1729 <idnac1729@gmail.com>
2026-08-26 16:44:48 +0530

    Time: 55 ms (5.04%) | Memory: 25.5 MB (5.79%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    int onesCount(const string &s)
+    {
+        int ones = 0;
+        for(char c : s)
+        {
+            if(c=='1')
+            ones++;
+        }
+        return ones;
+    }
+
+    string shortestBeautifulSubstring(string s, int k) {
+        unordered_map<int, set<string>> beautiful;
+        for(int i = 0; i < s.size(); i++)
+        {
+            for(int j = i; j < s.size(); j++)
+            {
+                const string &st = s.substr(i, j-i+1);
+                int count = onesCount(st);
+
+                if(count == k)
+                {
+                beautiful[j-i+1].insert(st);
+                //break;
+                }
+
+                //else if(count > k)
+                //break;
+            }
+        }
+
+        if(beautiful.empty())
+        return "";
+
+        int smallest = INT_MAX;
+        for(auto &a : beautiful)
+        {
+            smallest = min(smallest, a.first);
+        }
+
+        const set<string> result = beautiful[smallest];
+        string ans = *result.begin();
+
+
+
+        return ans;
+    }
+};
