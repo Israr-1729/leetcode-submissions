267d8c6c5b4bbde965479281d2e697221d38b7ed
Israr-1729 <idnac1729@gmail.com>
2026-08-29 00:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (55.29%) - LeetSync

---
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int columns = grid[0].size();
+
+        int currMax = 0;
+
+        for(int i = 0; i < rows-2; i++)
+        {
+            for(int j = 0; j < columns-2; j++)
+            {
+                int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2];
+
+                currMax = max(currMax, sum);
+            }
+        }
+        return currMax;
+    }
+};
