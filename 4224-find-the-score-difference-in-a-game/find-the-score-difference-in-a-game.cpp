766adab4e8f0cd3a55442757d34648c3fafc5737
Israr-1729 <idnac1729@gmail.com>
2026-08-29 12:17:17 +0530

    Time: 451 ms (5.23%) | Memory: 40.9 MB (80.56%) - LeetSync

---
+++ 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int scoreDifference(vector<int>& nums) {
+        bool ATakes = true;
+        int scoreA = 0; int scoreB = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] % 2 == 1)
+            {
+                ATakes = !ATakes;
+            }
+
+            if((i+1) % 6 == 0) 
+            {
+                ATakes = !ATakes;
+            }
+
+            if(ATakes)
+            {
+                scoreA += nums[i];
+            }
+
+            else if(!ATakes)
+            {
+                scoreB += nums[i];
+            }
+
+            cout<<scoreA<<" "<<scoreB<<"\n";
+        }
+        return (scoreA-scoreB);
+    }
+};
