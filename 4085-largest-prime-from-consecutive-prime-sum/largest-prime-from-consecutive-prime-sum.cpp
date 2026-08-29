0dca7a3c0742ae4c9128e027c8d1564c07e60504
Israr-1729 <idnac1729@gmail.com>
2026-08-29 09:40:52 +0530

    Time: 230 ms (72.78%) | Memory: 41.3 MB (36.98%) - LeetSync

---
+++ 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_set<int> primeNumbersSum;
+
+    bool isPrime(int n)
+    {
+        if(n == 2)
+        return true;
+
+        if(n < 2 || n%2 == 0)
+        return false;
+
+        for(int i = 3; i * i <= n; i += 2)
+        {
+            if(n % i == 0)
+            return false;
+        }
+
+        return true;
+    }
+    void fillPrimeNumbers()
+    {
+        int runningSum = 0;
+        for(int i = 1; i <= 50000; i++)
+        {
+            if(isPrime(i))
+            {
+                runningSum += i;
+                primeNumbersSum.insert(runningSum);
+            }
+
+            if(runningSum >= 500000)
+            break;
+        }
+    }
+
+    int largestPrime(int n) {
+        fillPrimeNumbers();
+        while(n >= 0)
+        {
+            if(primeNumbersSum.contains(n) && isPrime(n))
+            return n;
+            n--;
+        }
+        return 0;
+    }
+};
