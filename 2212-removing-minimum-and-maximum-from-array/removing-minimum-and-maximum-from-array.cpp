f8167d0641555f8674250672d63398d72a5e4380
Israr-1729 <idnac1729@gmail.com>
2026-08-30 08:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 92.2 MB (4.89%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int minIDX = -1; int maxIDX = -1;
+        int minm = INT_MAX; int maxm = INT_MIN;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] >= maxm)
+            {
+                maxm = nums[i];
+                maxIDX = i;
+            }
+
+            if(nums[i] <= minm)
+            {
+                minm = nums[i];
+                minIDX = i;
+            }
+        }
+        
+
+        int cuttingFromLeft = max(maxIDX, minIDX) + 1;
+        int cuttingFromRight = nums.size() - min(maxIDX, minIDX);
+        int cuttingFromBoth = (min(maxIDX, minIDX) + 1) + (nums.size() - max(maxIDX, minIDX));
+
+
+        /*cout<<cuttingFromLeft<<" "<<cuttingFromRight<<" "<<cuttingFromBoth<<"\n";
+        cout<<minm<<" "<<maxm<<"\n";
+        cout<<minIDX<<" "<<maxIDX<<"\n";
+        cout<<(min(maxIDX, maxIDX) + 1)<<" "<<(nums.size() - max(maxIDX, minIDX))<<"\n";*/
+        return min(min(cuttingFromLeft, cuttingFromRight), cuttingFromBoth);
+    }
+};
