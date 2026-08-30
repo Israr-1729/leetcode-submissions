10f0e76b3eec54aa0261a9fa283c826330c7c61b
Israr-1729 <idnac1729@gmail.com>
2026-08-30 14:48:56 +0530

    Time: 24 ms (5.70%) | Memory: 48.3 MB (11.91%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.rbegin(), g.rend());
+        sort(s.rbegin(), s.rend());
+        int sIDX = 0;
+        int count = 0;
+
+        for(int i = 0; i < g.size() && sIDX < s.size(); i++)
+        {
+            if(s[sIDX] >= g[i])
+            {
+            count++;
+            sIDX++;
+            }
+        } 
+
+        return count;       
+    }
+};
