a4f38362337114c712b5d817d1f92e726dde174d
Israr-1729 <idnac1729@gmail.com>
2026-09-02 00:37:00 +0530

    Time: 7 ms (53.65%) | Memory: 90.5 MB (5.07%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void lot(TreeNode* root, vector<int> &ans)
+    {
+        if(root == nullptr)
+        return;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int levelSize = q.size();
+            for(int i = 0; i < levelSize; i++)
+            {
+                TreeNode* current = q.front();
+                q.pop();
+
+                ans.push_back(current->val);
+
+                if(current->left) q.push(current->left);
+                if(current->right) q.push(current->right);
+            }
+        }
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> nums1; vector<int> nums2;
+        lot(root1, nums1);
+        lot(root2, nums2);
+
+        vector<int> result;
+        for(int i : nums1)
+        {
+            result.push_back(i);
+        }
+
+        for(int i : nums2)
+        {
+            result.push_back(i);
+        }
+
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
