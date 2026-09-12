7e2b0d6109ca6f9f9502fe2312bde05571e278a2
Israr-1729 <idnac1729@gmail.com>
2026-09-13 03:34:56 +0530

    Time: 3 ms (27.80%) | Memory: 96 MB (18.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,46 +1,18 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> combined;
+        for(int i : nums1)
+        combined.push_back(i);
 
-        int size1 = nums1.size();
-        int size2 = nums2.size();
-        vector<int> sortedCommon(size1 + size2);
-        int entries = 0;
-        int i = 0;
-        int j = 0;
-                 while (i < size1 && j < size2) {
-                if (nums1[i] <= nums2[j]) {
-                    sortedCommon[entries] = nums1[i];
-                    entries++;
-                    i++;
-                }
+        for(int i : nums2)
+        combined.push_back(i);
 
-                else {
-                    sortedCommon[entries] = nums2[j];
-                    entries++;
-                    j++;
-                }
-            }
+        sort(combined.begin(), combined.end());
 
-            while (j < size2) {
-                sortedCommon[entries] = nums2[j];
-                entries++;
-                j++;
-            }
-
-            while (i < size1) {
-                sortedCommon[entries] = nums1[i];
-                entries++;
-                i++;
-            }
-
-            int sizeCommon = size1 + size2;
-            if (sizeCommon % 2 == 0) {
-                return (sortedCommon[sizeCommon / 2] +
-                        sortedCommon[(sizeCommon / 2) - 1]) /
-                       2.0;
-            } else {
-                return (double)sortedCommon[sizeCommon / 2];
-            }
-        }
-    };
+        if(combined.size() % 2 != 0)
+        return (double)combined[combined.size()/2];
+        return ((double)(combined[(combined.size()-1)/2] + (double)combined[(combined.size()/2)])) / 2.0;
+        
+    }
+};
